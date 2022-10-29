72c3b085db20b584b938ae38c84c4ec64df36201
Mayo <mayolito.waje08@gmail.com>
2022-10-29 12:18:27 +0800

    set script for sketchpad size interactivity

--- script.js
+++ script.js
@@ -6,7 +6,32 @@ const changeSketchpadSizeLabelText = event => {
   textContainer.innerText = text;
 }
 
-window.addEventListener("DOMContentLoaded", event => {
+const initializeGrid = (size = 16) => {
+  const sketchpad = document.getElementById("sketchpad");
+  sketchpad.innerHTML = "";
+  sketchpad.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+
+  const totalPixelCount = size * size;
+  for (let i = 0; i < totalPixelCount; i++) {
+    const pixel = document.createElement("div");
+    pixel.setAttribute("class", "pixel");
+
+    sketchpad.appendChild(pixel);
+  }
+};
+
+const setSketchpadSize = event => {
   const rangeInput = document.querySelector("input#sketchpad-size");
-  rangeInput.addEventListener("input", changeSketchpadSizeLabelText)
+  const size = Number(rangeInput.value);
+  initializeGrid(size);
+};
+
+window.addEventListener("DOMContentLoaded", event => {
+  initializeGrid();
+
+  const rangeInput = document.querySelector("input#sketchpad-size");
+  rangeInput.addEventListener("input", changeSketchpadSizeLabelText);
+
+  const confirmSketchpadSizeButton = document.querySelector("button#apply-sketchpad-size");
+  confirmSketchpadSizeButton.addEventListener("click", setSketchpadSize);
 });
